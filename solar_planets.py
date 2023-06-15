9c2d4cbe92d80b5022aef0a0688175b2425039e5
nscoding123 <76574874+nscoding123@users.noreply.github.com>
2023-06-15 17:21:34 -0500

    Add files via upload

---
+++ solar_planets.py
@@ -0,0 +1,86 @@
+# import the opencv library
+import cv2
+img = cv2.imread('solar-system.jpg') 
+
+while(True):
+    cv2.imshow('output',img)
+
+    key = cv2.waitKey(1)
+
+    if key == 32:
+        break
+
+cv2.putText(img,
+            "Sun",
+            (20,300),
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (255,255,255)
+            )
+
+cv2.putText(img,
+            "Mercury",
+            (40,300),
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (255,255,255)
+            )
+
+cv2.putText(img,
+            "Venus",
+            (70,300),
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (255,255,255)
+            )
+
+cv2.putText(img,
+            "Earth",
+            (90,300),
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (255,255,255)
+            )
+
+cv2.putText(img,
+            "Mars",
+            (110,300),
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (255,255,255)
+            )
+
+cv2.putText(img,
+            "Jupiter",
+            (140,300),
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (255,255,255)
+            )
+
+cv2.putText(img,
+            "Saturn",
+            (200,300),
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (255,255,255)
+            )
+
+cv2.putText(img,
+            "Uranus",
+            (250,300),
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (255,255,255)
+            )
+
+cv2.putText(img,
+            "Neptune",
+            (300,300),
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (255,255,255)
+            )
+
+cv2.imwrite('Solar_systemwithname.jpg',img)
+
